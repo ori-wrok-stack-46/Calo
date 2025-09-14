8d6de0544387dfda43386ca861bbfee3b75ec600
ori-wrok-stack-46 <hoiuyq@gmail.com>
2025-09-14 15:02:09 +0300

    Delete client directory

--- client/components/loaders/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as ElementLoader } from "../ElementLoader";
-export { default as ButtonLoader } from "../ButtonLoader";
-export { default as CardLoader } from "../CardLoader";
-export { default as ErrorDisplay } from '../ErrorDisplay';
-export { default as LoadingScreen } from '../LoadingScreen';
--- client/components/menu/index.tsx
+++
@@ -1,4 +0,0 @@
-export { MenuCreator } from "./MenuCreator";
-export { MealDetailView } from "./MealDetailView";
-export { MealsListView } from "./MealsListView";
-export { EnhancedMenuCreator } from "./EnhancedMenuCreator";
